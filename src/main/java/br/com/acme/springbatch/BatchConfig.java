59fe8a3555125c467c898305f2ea18f8890247df
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-01-23 10:32:35 -0300

    feat: add step to count  or

--- src/main/java/br/com/acme/springbatch/BatchConfig.java
+++ src/main/java/br/com/acme/springbatch/BatchConfig.java
@@ -1,5 +1,8 @@
 package br.com.acme.springbatch;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -10,6 +13,9 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -22,10 +28,11 @@ public class BatchConfig {
     int count = 0;
 
     @Bean
-    public Job job(JobRepository jobRepository, Step step, Step stepCount) {
+    public Job job(JobRepository jobRepository, Step step, Step stepCount, Step imprimeParImparStep) {
         return new JobBuilder("job", jobRepository)
                 .start(step)
                 .next(stepCount)
+                .next(imprimeParImparStep)
                 .incrementer(new RunIdIncrementer())
                 .build();
     }
@@ -62,6 +69,31 @@ public class BatchConfig {
                 }, transactionManager)
                 .allowStartIfComplete(true)
                 .build();
+    }
+
+    @Bean
+    public Step imprimeParImparStep(
+            JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("imprimeParImparStep", jobRepository)
+                .<Integer, String>chunk(1, transactionManager)
+                .reader(contaAteDezReader())
+                .processor(parOuImparProcessor())
+                .writer(imprimeWriter())
+                .build();
+    }
+
+    public IteratorItemReader<Integer> contaAteDezReader() {
+        List<Integer> contaAteDez = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        return new IteratorItemReader<Integer>(contaAteDez.iterator());
+    }
+
+    public FunctionItemProcessor<Integer, String> parOuImparProcessor() {
+        return new FunctionItemProcessor<>(item -> item % 2 == 0 ? String.format("O número %s é par", item)
+                : String.format("O número %s é impar", item));
+    }
+
+    public ItemWriter<String> imprimeWriter() {
+        return itens -> itens.forEach(System.out::println);
     }
 
 }
